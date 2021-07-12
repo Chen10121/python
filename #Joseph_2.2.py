07e50aba0b4190f0f58d48cfd054f1510ee632c2
Chen10121 <86713869+Chen10121@users.noreply.github.com>
2021-07-12 17:26:16 +0800

    Add files via upload

--- #Joseph_2.2.py
+++ #Joseph_2.2.py
@@ -1,6 +1,35 @@
 #Joseph_2.2
 import random
 
+#定义约瑟夫环类型
+class Joseph:
+    def __init__(self,josephlist):          #josephlist由实例化的对象列表提供
+        self.josephlist = josephlist
+
+    def Josephring(self,step,start):        #Josephring实现原列表按照约瑟夫环顺序的重新排序，并存储在list_joseph中
+        assert step > 0,'Step size out of range!'
+        assert 0 < start < 12,'Start position out of range!'
+        list_rotate = self.josephlist[start-1:len(self.josephlist)+1]+self.josephlist[0:start-1]
+        list_sum = [0]*len(list_rotate)
+        sum = len(list_rotate)
+        counter = 1
+        subscript = 0
+        list_joseph = []
+        while sum > 1:
+            if subscript == len(list_sum) - 1:
+                subscript = -1
+            subscript += 1
+            if list_sum[subscript] != 1:
+                if counter == step:
+                    list_sum[subscript] = 1
+                    list_joseph.append(self.josephlist[subscript])
+                    counter = 1
+                    sum -=1
+                else:
+                    counter +=1
+        list_joseph.append(self.josephlist[0])
+        return list_joseph
+
 #创建对象的类
 class Person:
     def __init__(self,name,stu_number,gender):
@@ -20,42 +49,9 @@ joslist = list(range(0,11))
 for i in range(0,11):
     joslist[i] = globals()[list_name[i]]
 
-#定义约瑟夫环类型
-class Joseph:
-    def __init__(self,josephlist):          #josephlist由实例化的对象列表提供
-        self.josephlist = josephlist
-
-    def Josephring(self,step,start):        #Josephring实现原列表按照约瑟夫环顺序的重新排序，并存储在list_joseph中
-        assert step > 0,'Step size out of range!'
-        assert 0 < start < 12,'Start position out of range!'
-        list_sum = self.josephlist[start-1:len(self.josephlist)+1]+self.josephlist[0:start-1]
-        sum = len(self.josephlist)
-        counter = 1
-        subscript = 0
-        list_pop = []
-        list_joseph = []
-        while sum > 1:
-            if counter < step:
-                counter += 1
-            else:
-                counter = 1
-                sum -= 1
-                list_pop.append(list_sum[subscript])
-                list_joseph.append(list_sum[subscript])
-            if subscript < len(list_sum):
-                if subscript > len(list_sum)-step-1:
-                    list_sum = list_sum[subscript+1:len(list_sum)] + list_sum[0:subscript+1]
-                    for i in range(len(list_pop)):
-                        list_sum.remove(list_pop[i])
-                    subscript = -1
-                    list_pop = []
-                subscript += 1
-        list_joseph.append(list_sum[0])
-        return list_joseph
 
 step = int(input('Step size:'))
 start = int(input('Start position:'))
 j = Joseph(joslist)    
 for i in j.Josephring(step,start):
     print('Name:',i.name,'Student number:',i.stu_number,'Gender:',i.gender)
-
